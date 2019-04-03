630b19842aaf672dcf1c5380e0db8770a11f72ea
Alexzanderk <kotsarev.a@gmail.com>
2019-04-03 10:03:33 +0300

    add btn gAuth

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,19 +1,31 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import { withStyles } from '@material-ui/core/styles';
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+    const onSuccess = googleUser => {
+        const idToken = googleUser.getAuthResponse().id_token;
+        console.log(idToken);
+    };
+
+    return (
+        <GoogleLogin
+            clientId="616300225060-rnr9bpeu1m9lu95lea6iii4l5tgup11l.apps.googleusercontent.com"
+            onSuccess={onSuccess}
+            isSignedIn={true}
+        />
+    );
 };
 
 const styles = {
-  root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
-  }
+    root: {
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
 };
 
 export default withStyles(styles)(Login);
